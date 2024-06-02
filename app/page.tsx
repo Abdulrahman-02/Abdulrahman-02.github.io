7dd5d65ce9d36554af11a02408c762121f7bbd14
Abderrahmane BENAISSA <76059937+Abdulrahman-02@users.noreply.github.com>
2024-06-02 01:03:36 +0000

    feat: Add Text Generate Effect , Button & a Floating Nav.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/Hero"
+import { FloatingNav } from "@/components/ui/floatingNav";
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex
+    <main className="relative bg-black-100 flex
      justify-center items-center flex-col overflow-hidden
      mx-auto sm:px-10 px-5 ">
       <div className="max-w-7xl w-full">
+        {/* <FloatingNav navItems={[
+          {name: 'Home', link: '#home', icon: <Home />},
+        ]} /> */}
         <Hero />
       </div>  
     </main>
---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MagicButton = () => {
+  return (
+    <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-300 transition-colors  focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          Show my work
+    </button>
+  )
+}
+
+export default MagicButton
+
+
+        
+       
+        
+      
