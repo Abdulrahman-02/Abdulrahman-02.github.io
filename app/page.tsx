5807e6462ec8827195efd8f9d1f8359159e08fda
Abderrahmane BENAISSA <76059937+Abdulrahman-02@users.noreply.github.com>
2024-06-06 07:02:00 +0000

    feat: Add Grid component for displaying items in a grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero"
 import { FloatingNav } from "@/components/ui/floatingNav";
 
@@ -11,6 +12,7 @@ export default function Home() {
           {name: 'Home', link: '#home', icon: <Home />},
         ]} /> */}
         <Hero />
+        <Grid />
       </div>  
     </main>
   );
